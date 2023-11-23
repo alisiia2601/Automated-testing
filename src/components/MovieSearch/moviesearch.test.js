0de030f84903a18d1d71c7c0930dfdf2fe3e5d96
alisiia2601 <alisiia.ryzhykova@icloud.com>
2023-11-23 02:20:19 +0100

    tests added

--- src/components/MovieSearch/moviesearch.test.js
+++ src/components/MovieSearch/moviesearch.test.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from "react";
 import { render, screen, fireEvent } from '@testing-library/react';
 import MovieSearch from '.';
 
@@ -68,14 +68,76 @@ describe('An input field for movie search', () => {
     expect(button).toBeInTheDocument();
   });
 
-
   test('does not render an element with an invalid role', () => {
     render(<MovieSearch />);
     const invalidElement = screen.queryByRole('invalidRole');
     expect(invalidElement).toBeNull();
   });
 
+  test('state changes on input change', () => {
+    render(<MovieSearch />);
+    const searchInput = screen.getByPlaceholderText('Enter movie title');
+
+    fireEvent.change(searchInput, { target: { value: 'Inception' } });
+    expect(screen.getByDisplayValue('Inception')).toBeInTheDocument();
+  });
+
+  test('gets all buttons by text content', () => {
+    render(
+      <div>
+        <button type="button">Click Me</button>
+        <button type="button">Submit</button>
+      </div>
+    );
+  
+    const buttons = screen.getAllByRole('button', { name: /click/i });
+    expect(buttons.length).toBe(1);
+  });
+
+  test('queries for an element by title attribute', () => {
+    render(
+      <div>
+        <p title="important-paragraph">This is an important paragraph.</p>
+      </div>
+    );
+  
+    const element = screen.queryByTitle('important-paragraph');
+    expect(element).toBeInTheDocument();
+  });
+
+test('gets all button elements by role', () => {
+    render(
+      <MovieSearch />
+    );
+  
+    const buttons = screen.getAllByRole('button');
+    expect(buttons.length).toBe(1); 
+  });
   
 
-  
+  test('state changes on form submission', () => {
+    let submittedSearchTerm = '';
+    const onSubmit = (searchTerm) => {
+      submittedSearchTerm = searchTerm;
+    };
+
+    render(<MovieSearch onSubmit={onSubmit} />);
+    const searchInput = screen.getByPlaceholderText('Enter movie title');
+    const searchButton = screen.getByRole('button', { name: /search/i });
+
+    fireEvent.change(searchInput, { target: { value: 'Interstellar' } });
+    fireEvent.click(searchButton);
+  });
+
+  test('state does not change on form submission with empty search term', () => {
+    let submittedSearchTerm = '';
+    const onSubmit = (searchTerm) => {
+      submittedSearchTerm = searchTerm;
+    };
+
+    render(<MovieSearch onSubmit={onSubmit} />);
+    const searchButton = screen.getByRole('button', { name: /search/i });
+
+    fireEvent.click(searchButton);
+  });
 });
